e3bfa460b6bd06c11bbd9aa1dfc577077d10e3fe
Mat Jordan <mat@utk.edu>
2019-05-10 21:27:39 -0400

    Build libguides API authorizer to retrieve token.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = {
+  endpoint: 'https://lgapi-us.libapps.com/1.2/oauth/token',
+  client_id: "406",
+  client_secret: "70a2cdb608f280d3b85a0f97f4b8bad3",
+}
+
+class App extends Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state ={
+      authorization: null
+    }
+
+  }
+
+  libAuthorize() {
+
+    let {endpoint, client_id, client_secret} = api
+
+    const credientals =
+        'client_id=' + client_id +
+        '&client_secret=' + client_secret +
+        '&grant_type=client_credentials'
+
+    let post = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: credientals
+    };
+
+    fetch(endpoint, post)
+        .then(response => response.json())
+        .then(data => {
+          if (data !== null) {
+            this.setState({
+              authorization: data
+            })
+          }
+        })
+
+  }
+
+  componentDidMount() {
+    this.libAuthorize()
+  }
+
+  render() {
+    return (
+        <div className="App">
+          nada
+        </div>
+    );
+  }
 }
 
 export default App;
