e2d75d2217d2818778c9733dcc20144ae7140274
Mat Jordan <mat@utk.edu>
2019-05-16 17:51:26 -0400

    Adding additional sample data, building slide effect for assets.

--- src/data/DataGuide.js
+++ src/data/DataGuide.js
@@ -39,6 +39,71 @@ export const DataGuide = {
                     }
                 ]
             }
+        },
+        {
+            "slug" : "chemistry",
+            "title" : "Chemistry",
+            "url": "https://libguides.utk.edu/path",
+            "disciplines" : [
+                {
+                    "name": "Anatomy Table",
+                    "url": "https://libguides.utk.edu/path"
+                },
+                {
+                    "name": "Biology",
+                    "url": "https://libguides.utk.edu/path"
+                },
+                {
+                    "name": "Environment Studies",
+                    "url": "https://libguides.utk.edu/path"
+                }
+            ],
+            "assets" : {
+                "databases" : [
+                    {
+                        "name": "Biological Abstracts (1926-present)",
+                        "url": "https://libguides.utk.edu/path",
+                        "description": "The most important database for the life sciences. For more information see the video tutorials and Quick Reference Card."
+                    },
+                    {
+                        "name": "Biology Databases thru Web of Science",
+                        "url": "https://libguides.utk.edu/path",
+                        "description": "Search several top biological sciences databases simultaneously: Biological Abstracts, CABI, Zoological Record, Food Science and Technology Abstracts, and Web of Science."
+                    },
+                    {
+                        "name": "Biology Databases thru Web of Science",
+                        "url": "https://libguides.utk.edu/path",
+                        "description": "Search several top biological sciences databases simultaneously: Biological Abstracts, CABI, Zoological Record, Food Science and Technology Abstracts, and Web of Science."
+                    },
+                    {
+                        "name": "Biology Databases thru Web of Science",
+                        "url": "https://libguides.utk.edu/path",
+                        "description": "Search several top biological sciences databases simultaneously: Biological Abstracts, CABI, Zoological Record, Food Science and Technology Abstracts, and Web of Science."
+                    }
+                ],
+                "additional" : [
+                    {
+                        "name": "Biological Abstracts (1926-present)",
+                        "url": "https://libguides.utk.edu/path",
+                        "media": "https://path.to/file.jpg"
+                    },
+                    {
+                        "name": "Biological Abstracts (1926-present)",
+                        "url": "https://libguides.utk.edu/path",
+                        "media": "https://path.to/file.jpg"
+                    },
+                    {
+                        "name": "Biological Abstracts (1926-present)",
+                        "url": "https://libguides.utk.edu/path",
+                        "media": "https://path.to/file.jpg"
+                    },
+                    {
+                        "name": "Biological Abstracts (1926-present)",
+                        "url": "https://libguides.utk.edu/path",
+                        "media": "https://path.to/file.jpg"
+                    }
+                ]
+            }
         }
     ]
 };
