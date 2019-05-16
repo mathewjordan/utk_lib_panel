6c6f2f4a3a168acea4c27f7d5f428f7044f72d1c
Mat Jordan <mat@utk.edu>
2019-05-16 09:29:36 -0400

    Update sample JSON schema.

--- src/components/DataGuide.js
+++ src/components/DataGuide.js
@@ -1,34 +1,44 @@
 export const DataGuide = {
-    "databases": [
+    "subjects" : [
         {
-            "name": "Biological Abstracts (1926-present)",
-            "description": "The most important database for the life sciences. For more information see the video tutorials and Quick Reference Card."
-        },
-        {
-            "name": "Biology Databases thru Web of Science",
-            "description": "Search several top biological sciences databases simultaneously: Biological Abstracts, CABI, Zoological Record, Food Science and Technology Abstracts, and Web of Science."
-        }
-    ],
-    "additional": [
-        {
-            "name": "deserunt",
-            "description": "adipisicing"
-        },
-        {
-            "name": "deserunt",
-            "description": "mollit"
-        },
-        {
-            "name": "mollit",
-            "description": "deserunt"
-        },
-        {
-            "name": "consequat",
-            "description": "sint"
-        },
-        {
-            "name": "fugiat",
-            "description": "ut"
+            "slug" : "biology",
+            "title" : "Biological Sciences",
+            "url": "https://libguides.utk.edu/path",
+            "disciplines" : [
+                {
+                    "name": "Anatomy Table",
+                    "url": "https://libguides.utk.edu/path"
+                },
+                {
+                    "name": "Biology",
+                    "url": "https://libguides.utk.edu/path"
+                },
+                {
+                    "name": "Environment Studies",
+                    "url": "https://libguides.utk.edu/path"
+                }
+            ],
+            "data" : {
+                "databases" : [
+                    {
+                        "name": "Biological Abstracts (1926-present)",
+                        "url": "https://libguides.utk.edu/path",
+                        "description": "The most important database for the life sciences. For more information see the video tutorials and Quick Reference Card."
+                    },
+                    {
+                        "name": "Biology Databases thru Web of Science",
+                        "url": "https://libguides.utk.edu/path",
+                        "description": "Search several top biological sciences databases simultaneously: Biological Abstracts, CABI, Zoological Record, Food Science and Technology Abstracts, and Web of Science."
+                    }
+                ],
+                "additional" : [
+                    {
+                        "name": "Biological Abstracts (1926-present)",
+                        "url": "https://libguides.utk.edu/path",
+                        "media": "https://path.to/file.jpg"
+                    }
+                ]
+            }
         }
     ]
 };
