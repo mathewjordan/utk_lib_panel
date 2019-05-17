107d2b20c1c325dfe6b78cd2043eb844115d8c6e
Mat Jordan <mat@utk.edu>
2019-05-17 13:25:02 -0400

    Update subject guide selector.

--- src/components/SubjectGuide.js
+++ src/components/SubjectGuide.js
@@ -7,34 +7,24 @@ class SubjectGuide extends Component {
 
         const {guides} = this.props
 
-        console.log(guides)
+        const options = guides.map((item, key) => (
+            {
+                key: key,
+                text: item.name,
+                value: item.url
+            }
+        ))
 
         if (guides) {
             return (
                 <React.Fragment>
                     <Dropdown
                         text='Find Research Guides'
-                        icon='angle down'
-                        floating
-                        labeled
                         direction='left'
+                        options={options}
                         className='icon utk-research-guide--browse'
                     >
-                        <Dropdown.Menu>
-                            <Input icon='search'
-                                   iconPosition='left'
-                                   className='search' />
-                            <Dropdown.Menu scrolling>
-                                {guides.map((item, key) => (
-                                    <Dropdown.Item key={key}
-                                                   value={item.url}
-                                                   text={item.name} />
-                                ))}
-                            </Dropdown.Menu>
-                        </Dropdown.Menu>
                     </Dropdown>
-                    {/*<p className="description">Resources for <a href="#">Anatomy Table</a>, <a*/}
-                    {/*href="#">Biology</a> and <a href="#">Environment Studies</a>.</p>*/}
                 </React.Fragment>
             )
         } else {
