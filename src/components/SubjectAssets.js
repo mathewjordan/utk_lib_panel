ace1c2f1f876676923e823b545b4ea0074217c76
Mat Jordan <mat@utk.edu>
2019-06-06 17:34:39 -0400

    Simplification of ui for assets.

--- src/components/SubjectAssets.js
+++ src/components/SubjectAssets.js
@@ -1,57 +1,81 @@
 import React, {Component} from 'react';
-import { Tab } from 'semantic-ui-react'
+import { Tab, Menu, Label } from 'semantic-ui-react'
 
 import SubjectDatabase from "./SubjectDatabase";
 import SubjectMedia from "./SubjectMedia";
 import SubjectGuide from "./SubjectGuide";
 
 class SubjectAssets extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state ={
+            id: this.props.subjectHeader.id,
+            build: false,
+            databases: []
+        }
+    }
+
+    componentWillReceiveProps() {
+        this.setState({
+            build: false
+        });
+    }
+
+    buildDatabasePane (data, id) {
+
+        let build = this.state.build;
+
+        if (data.featured_databases.length !== 0 && build === false) {
+
+            let panes = [
+                {
+                    menuItem: (
+                        <Menu.Item key='messages'>
+                            Databases<Label>{data.total_databases}</Label>
+                        </Menu.Item>
+                    ),
+                    render: () => <Tab.Pane attached={false}><div className="utk-subject-guide--databases utk-subject-guide--asset">
+                        <div className="utk-subject-guide--asset--header">
+                            <h5>Recommended Databases</h5>
+                            <a>View All Databases <span className='icon-open-right'></span></a>
+                        </div>
+                        <ul>
+                            {data.featured_databases.map((item, key) => (
+                                <SubjectDatabase key={key}
+                                                 unique={id + '-' +  key}
+                                                 instance={key}
+                                                 item={item}/>
+                            ))}
+                        </ul>
+                    </div></Tab.Pane>
+                    }
+                ]
+
+            this.setState({
+                id: id,
+                build: true,
+                databases: panes
+            });
+        }
+    }
+
     render() {
 
         const {id, title} = this.props.subjectHeader
-        const {associated_libguides, featured_databases, total_databases} = this.props.subjectData
+        const {subjectData} = this.props
 
-        const panes = [
-            { menuItem: 'Databases', render: () => <Tab.Pane attached={false}>Tab 1 Content</Tab.Pane> },
-            { menuItem: 'Research Guides', render: () => <Tab.Pane attached={false}>Tab 2 Content</Tab.Pane> },
-            { menuItem: 'More Resources', render: () => <Tab.Pane attached={false}>Tab 3 Content</Tab.Pane> }
-        ]
-
-        console.log(featured_databases)
+        this.buildDatabasePane(subjectData, id)
 
         return (
             <div className={`utk-subject-guide`} data-subject-id={id}>
                 <div className="utk-subject-guide--header">
                     <h4>{title}</h4>
-                    {/*<SubjectGuide guides={associated_libguides} />*/}
                 </div>
-                <Tab menu={{ secondary: true }} panes={panes} className="utk-subject-guide--assets" />
-                {/*<div className="utk-subject-guide--databases utk-subject-guide--asset">*/}
-                    {/*<div className="utk-subject-guide--asset--header">*/}
-                        {/*<h5>Recommended Databases</h5>*/}
-                        {/*<a>View All <span>{total_databases}</span></a>*/}
-                    {/*</div>*/}
-                    {/*<ul>*/}
-                        {/*{featured_databases.map((item, key) => (*/}
-                            {/*<SubjectDatabase key={key}*/}
-                                             {/*unique={id + '-' +  key}*/}
-                                             {/*instance={key}*/}
-                                             {/*item={item}/>*/}
-                        {/*))}*/}
-                    {/*</ul>*/}
-                {/*</div>*/}
-                {/*<div className="utk-subject-guide--media utk-subject-guide--asset">*/}
-                    {/*<div className="utk-subject-guide--asset--header">*/}
-                        {/*<h5>Other Helpful Resources</h5>*/}
-                    {/*</div>*/}
-                    {/*<ul>*/}
-                        {/*{assets.additional.map((item, key) => (*/}
-                            {/*<SubjectMedia key={key}*/}
-                                          {/*instance={key}*/}
-                                          {/*item={item}/>*/}
-                        {/*))}*/}
-                    {/*</ul>*/}
-                {/*</div>*/}
+                <Tab menu={{ secondary: true }}
+                     panes={this.state.databases}
+                     className="utk-subject-guide--assets" />
             </div>
         )
     }
