5ef2c22f8f07051e1b995966171839871ae5234e
Mat Jordan <mat@utk.edu>
2019-07-01 09:51:03 -0400

    Make search on homepanel fully functional.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
 import { Tab, Button, Select, Input } from 'semantic-ui-react'
+import SearchForm from "./SearchForm";
+
+const onesearchOptions = [
+    { key: 'onesearch', text: 'One Search', value: 'onesearch' },
+    { key: 'ut-collections', text: 'UT Collections', value: 'ut-collections' },
+    { key: 'course-reviews', text: 'Course Reserves', value: 'course-reviews' },
+]
 
 class Search extends Component {
 
@@ -7,36 +14,14 @@ class Search extends Component {
 
         const panes = [
             { menuItem: 'OneSearch', render: () =>
-                <Tab.Pane attached={false}>
-                    <Input type='text'
-                           placeholder='Search for books, articles, and more...'
-                           action>
-                        <input />
-                        <Select compact
-                                icon
-                                className="icon-down-open"
-                                options={onesearchOptions}
-                                defaultValue='onesearch' />
-                        <Button type='submit'>Submit</Button>
-                    </Input>
-                </Tab.Pane>
+                <SearchForm selection='onesearch'
+                            options={onesearchOptions}
+                            placeholder='Search for books, articles, and more...' />
             },
             { menuItem: 'Our Website', render: () =>
-                <Tab.Pane attached={false}>
-                    <Input type='text'
-                           placeholder='Search our website for services and more...'
-                           action>
-                        <input />
-                        <Button type='submit'>Submit</Button>
-                    </Input>
-                </Tab.Pane>
+                <SearchForm selection='libraries'
+                            placeholder='Search our website for services and more...' />
             }
-        ]
-
-        const onesearchOptions = [
-            { key: 'onesearch', text: 'One Search', value: 'onesearch' },
-            { key: 'ut-collections', text: 'UT Collections', value: 'ut-collections' },
-            { key: 'course-reviews', text: 'Course Reserves', value: 'course-reviews' },
         ]
 
         return (
---
+++ src/components/SearchForm.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import {Button, Form, Input, Select, Tab} from "semantic-ui-react";
+
+const searchEndpoint = "https://www.lib.utk.edu/search/submit?go=1"
+
+class SearchForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            query: '',
+            option: null
+        };
+    }
+
+    getOptions = (options, defaultOptions) => {
+        if (options) {
+            return <Select compact
+                           icon
+                           className="icon-down-open"
+                           options={options}
+                           defaultValue={options[0].key}
+                           onChange={this.handleOptionChange} />
+        } else {
+            return null
+        }
+    }
+
+    handleInputChange = (e, {value}) => this.setState({ query : value })
+
+    handleOptionChange = (e, {value}) => this.setState({ option : value })
+
+    render() {
+
+        const {selection, placeholder, options, defaultOption} = this.props
+
+        return (
+            <Tab.Pane attached={false}>
+                <Form method="post"
+                      action={searchEndpoint}>
+                    <Input type='hidden'
+                           name='method'
+                           value={selection}/>
+                    <Input type='text'
+                           name="value"
+                           placeholder={placeholder}
+                           onChange={this.handleInputChange}
+                           action>
+                        <input />
+                        {this.getOptions(options, defaultOption)}
+                        <Button type='submit'>Submit</Button>
+                    </Input>
+                </Form>
+            </Tab.Pane>
+        )
+    }
+}
+
+export default SearchForm;
