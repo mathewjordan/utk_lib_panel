5c59291c4784c35eb3f4d7f20edbe11ad63a2643
Mat Jordan <mat@utk.edu>
2020-05-30 17:09:24 -0400

    Revise shipping/packaging to pantheon repo.

--- ship/ship-www.sh
+++ ship/ship-www.sh
@@ -1,9 +1,9 @@
 echo 'Shipping React Build to...';
-echo '~/Sites/dev/www/wp-content/themes/utk_lib_wp_theme/resources/assets/react/panel';
+echo '~/Sites/dev/utk-libraries/wp-content/themes/utk_lib_wp_theme/resources/assets/react/panel';
 
-rsync -av --delete dist/ ~/Sites/dev/www/wp-content/themes/utk_lib_wp_theme/resources/assets/react/panel
+rsync -av --delete dist/ ~/Sites/dev/utk-libraries/wp-content/themes/utk_lib_wp_theme/resources/assets/react/panel
 
-cd ~/Sites/dev/www/wp-content/themes/utk_lib_wp_theme/;
+cd ~/Sites/dev/utk-libraries/wp-content/themes/utk_lib_wp_theme/;
 yarn build;
 cd ~/Sites/react/utk_lib_panel
 
